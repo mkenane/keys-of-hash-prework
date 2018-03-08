4f3221e7bbda8d03167f3438bd9a0191614a9bef
Mor Kenane <morkenan@hotmail.com>
2018-03-08 22:38:14 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,16 +1,16 @@
 
 class Hash
-def keys_of(country, *countries)
-  allcountries = countries.unshift(country)
+  def keys_of(country, *countries)
+    allcountries = countries.unshift(country)
     match_array = []
     
     allcountries.each do |element|
-    self.each do |animal, origin|
-      if element == origin
-        match_array.push(animal)
+      self.each do |animal, origin|
+        if element == origin
+          match_array.push(animal)
+        end
       end
-    end
-  end 
+    end 
   match_array
   end 
 end 
